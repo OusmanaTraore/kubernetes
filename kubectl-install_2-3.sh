b975e55c4e5cb799232bb4e6ea6154f5fadf65fb
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-03-10 08:08:00 +0100

    Update kubectl-install_2-3.sh

--- kubectl-install_2-3.sh
+++ kubectl-install_2-3.sh
@@ -7,24 +7,24 @@ sleep 2
 echo "======      ETAPE 3/9   ========="
 echo " Vérification de la configuration du fichier /etc/docker/daemon.json"
 cat /etc/docker/daemon.json > $test_daemon
-if [ $test_daemon -ne $DAEMON_VAR ]
-  then
+if [ $test_daemon -ne $DAEMON_VAR ];
+then
     echo "ERREUR de configuration du fichier /etc/docker/daemon.json"
     echo "Corriger votre fichier puis relancer le script"
     echo "..."
     exit
-  elif
+else
     echo " Configuration du fichier /etc/docker/daemon.json === OK ==="
     sleep 2
     echo "======      ETAPE 4/9   ========="
     echo "Redémarrage du service docker"
 
     sudo systemctl restart docker
-    if [ $? -ne 0 ] 
-      then
+    if [ $? -ne 0 ]; 
+    then
         echo " ERREUR DE DEMARRAGE DOCKER"
         exit
-      elif
+    else
         sudo systemctl status  docker
         echo "======      ETAPE 5/9  ========="
         echo " Téléchargement des packages kubernetes"
@@ -46,11 +46,11 @@ if [ $test_daemon -ne $DAEMON_VAR ]
         echo "Installation de kubelet kubeadm kubectl kubernetes-cni"
         sleep 2
         sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
-        if [ $? -ne 0 ] 
-          then
+        if [ $? -ne 0 ]; 
+        then
             echo " ECHEC  étape installation kubeadm kubectl kubernetes-cli "
             exit
-          elif
+        else
             echo "======PLEASE ====="
             echo " Veuillez éditer le fichier /etc/default/kubelet et insérer le code "
             echo " suivant en remplacant l'adresse IP_ETH1 par celle de votre machine"
