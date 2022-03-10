1f416721090d1d3a7731a889055d98ac9a2c1959
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-03-10 06:59:12 +0100

    Update kubectl-install_1-3.sh

--- kubectl-install_1-3.sh
+++ kubectl-install_1-3.sh
@@ -5,7 +5,7 @@ IP_ETH1="192.168.50.20"
 
 echo "Adresse machine : $IP_ETH1 "
 echo "======Début installation======"
-echo "======      ETAPE 1/9  ========="
+echo "======      ETAPE 1/9  Désactivation de la swap ========="
 sleep 2
 ### Désactiver la swap  et la rendre persistente
 echo " Désactivation de la swap  et la rendre persistente "
@@ -13,9 +13,10 @@ echo " Désactivation de la swap  et la rendre persistente "
 sudo swapoff -a && sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 echo ""
 ###Installation de docker
-echo "======      ETAPE 2/9   ========="
+echo "======      ETAPE 2/9  Installation de docker ========="
 echo "  Début Installation de docker  "
 sleep 2
+echo "======      ETAPE 2/9  Installation de docker  ========="
 echo " 1/4 - installation des certificats"
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 echo ""
@@ -29,16 +30,22 @@ sudo apt update -y
 echo ""
 echo " 4/4 - installation de docker"
 echo ""
-echo "======      ETAPE 3/9   ========="
-echo " Création et configuration du fichier /etc/docker/daemon.json"
-sleep 2
-echo ""
-echo " Editer le fichier /etc/docker/daemon.json et insérer le code { "exec-opts": ["native.cgroupdriver=systemd"] } "
+if [ $? != 0 ]
+then
+  echo "ECHEC installation de docker "
+  exit 1
+elif
+  sleep 2
+  echo " installation de docker OK" 
+  echo "======      ETAPE 3/9   ========="
+  echo " Création et configuration du fichier /etc/docker/daemon.json"
+  sleep 2
+  echo ""
+  echo " Editer le fichier /etc/docker/daemon.json et insérer le code { "exec-opts": ["native.cgroupdriver=systemd"] } "
+  echo " Ensuite lancer le script kubectl-install_2-3.sh"
+  echo "..."
+fi 
 
-echo ""
-echo "======      ETAPE 4/9   ========="
-echo "Redémarrage du service"
-sudo systemctl restart docker
-sudo systemctl status docker
+
 
 
