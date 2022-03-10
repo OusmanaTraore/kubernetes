3b26c752d48fc1dfef0adffa5f140ea380f6d472
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-03-10 07:34:31 +0100

    Update kubectl-install_2-3.sh

--- kubectl-install_2-3.sh
+++ kubectl-install_2-3.sh
@@ -1,55 +1,67 @@
 #!/bin/bash
 
+IP_ETH1="192.168.50.20"
+DAEMON_VAR=`{ "exec-opts": ["native.cgroupdriver=systemd"] }`
+
 sleep 2
+echo "======      ETAPE 3/9   ========="
+echo " Vérification de la configuration du fichier /etc/docker/daemon.json"
+cat /etc/docker/daemon.json > $test_daemon
+if [ $test_daemon -ne $DAEMON_VAR ]
+  then
+    echo "ERREUR de configuration du fichier /etc/docker/daemon.json"
+    echo "Corriger votre fichier puis relancer le script"
+    echo "..."
+    exit
+  elif
+    echo " Configuration du fichier /etc/docker/daemon.json === OK ==="
+    sleep 2
+    echo "======      ETAPE 4/9   ========="
+    echo "Redémarrage du service docker"
 
-echo ""
-echo "======      ETAPE 4/9   ========="
-echo "Redémarrage du service docker"
+    sudo systemctl restart docker
+    if [ $? -ne 0 ] 
+      then
+        echo " ERREUR DE DEMARRAGE DOCKER"
+        exit
+      elif
+        sudo systemctl status  docker
+        echo "======      ETAPE 5/9  ========="
+        echo " Téléchargement des packages kubernetes"
+        sleep 2
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo systemctl restart docker
-if [ $? -ne 0 ] 
-then
-  echo " ERREUR DE DEMARRAGE DOCKER"
-  exit
-elif
-  sudo systemctl status  docker
+        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        bionic stabe"
 
-  echo "======      ETAPE 5/9  ========="
-  echo " Téléchargement des packages kubernetes"
-  sleep 2
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+        curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-  bionic stabe"
+        echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
 
-  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+        sudo mv ~/kubernetes.list /etc/apt/sources.list.d
+        sudo apt update
+        echo ""
 
-  echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
-
-  sudo mv ~/kubernetes.list /etc/apt/sources.list.d
-  sudo apt update
-  echo ""
-
-  echo "======      ETAPE 6/9   ========="
-  echo "Installation de kubelet kubeadm kubectl kubernetes-cni"
-  sleep 2
-  sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
-fi
-
-if [ $? -ne 0 ] 
-then
-  echo " ECHEC  étape installation kubeadm kubectl kubernetes-cli "
-  exit
-elif
-  echo "======PLEASE ====="
-  echo " Veuillez éditer le fichier /etc/default/kubelet et insérer le code "
-  echo " suivant en remplacant l'adresse IP_ETH1 par celle de votre machine"
-  echo ""
-  echo KUBELET_EXTRA_ARGS="--node-ip=$IP_ETH1"
-  sleep 2
-  echo ""
-  echo "Ensuite lancez le script kubectl-install_3-3.sh"
-  echo "..."
+        echo "======      ETAPE 6/9   ========="
+        echo "Installation de kubelet kubeadm kubectl kubernetes-cni"
+        sleep 2
+        sudo apt install -y kubelet kubeadm kubectl kubernetes-cni
+        if [ $? -ne 0 ] 
+          then
+            echo " ECHEC  étape installation kubeadm kubectl kubernetes-cli "
+            exit
+          elif
+            echo "======PLEASE ====="
+            echo " Veuillez éditer le fichier /etc/default/kubelet et insérer le code "
+            echo " suivant en remplacant l'adresse IP_ETH1 par celle de votre machine"
+            echo ""
+            echo KUBELET_EXTRA_ARGS="--node-ip=$IP_ETH1"
+            sleep 2
+            echo ""
+            echo "Ensuite lancez le script kubectl-install_3-3.sh"
+            echo "..."
+        fi
+    fi
 fi
   
   
