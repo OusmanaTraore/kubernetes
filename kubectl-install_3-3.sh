4b2fc1f96d69ac4af3c72bd20ce747161651b47a
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-03-10 14:44:24 +0100

    Update kubectl-install_3-3.sh

--- kubectl-install_3-3.sh
+++ kubectl-install_3-3.sh
@@ -1,51 +1,61 @@
 #!/bin/bash
-IP_ETH1="192.168.50.20"
+IP_ETH1="192.168.50.13"
 KUBELET_VAR="KUBELET_EXTRA_ARGS=\"--node-ip=$IP_ETH1\""
-test_kubelet=
+
 echo "======      ETAPE 7/9   ========="
 echo " configuration kubernetes"
 
 echo " Vérification de la configuration du fichier /etc/default/kubelet"
 sleep 3
 #sudo echo KUBELET_EXTRA_ARGS="--node-ip=$IP_ETH1" > "/etc/default/kubelet"
-sudo cat "/etc/default/kubelet" > test_kubelet
+test_kubelet=`sudo cat /etc/default/kubelet`
 
-if [ $ttest_kubelet -ne $KUBELET_VAR ];
+if [ $test_kubelet != $KUBELET_VAR ];
 then
-    echo "ERREUR de configuration du fichier /etc/docker/daemon.json"
-    echo "Corriger votre fichier puis relancer le script"
+    echo "ERREUR de configuration du fichier /etc/default/kubelet"
+    echo "Corrigez le  fichier puis relancer le script"
     echo "..."
     exit
 else
-
-if [ $? -ne  KUBELET_EXTRA_ARGS="--node-ip=$IP_ETH1"  ] ;
-then
-  echo "Défaut de configuration du fichier /etc/default/kubelet"
-  exit 
-else
+  echo "<=== Configuration du fichier /etc/default/kubelet ===>"
+  echo "===>...OK"
+  echo ""
   echo "======      ETAPE 8/9   ========="
   echo "Redémarrage des services daemon  et kubelet"
   sleep 2
 
   echo " 1/2 - Redémarrage daemon"
-  systemctl  daemon-reload
+  sudo systemctl  daemon-reload
+fi
   if [ $? -ne 0 ];
   then
     echo "ECHEC daemon-reload"
     exit
   else
-    sudo systemctl status daemon-reload
     sleep 2
     echo " 2/2 - Redémarrage kubelet"
     sudo systemctl restart kubelet
-    sudo systemctl status  kubelet
+    sudo systemctl status  kubelet | grep Active
     sleep 1
+        if  [[ `sudo systemctl status kubelet | grep loaded` ]];
+        then
+                sleep 2
+                echo ""
+                echo "========//\\========="
+                echo "installation réussie "
+                echo "========\\//=========" 
 
+        else
+
+                echo "========//\\=================="
+                echo " echec du démarrage de kubelet"
+                echo "========\\//==================" 
+        fi
     echo ""
-    echo ""
-    echo "======      ETAPE 9/9   ========="
-    echo " Initialisation de Kubeadm "
-    sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$IP_ETH1  --ignore-preflight-errors=NumCPU
+    sleep 2
+    echo "============================================"
+    echo "===== FIN de l'installation sur le node ===="
+    echo "============================================"
+    
    fi
 
-fi
