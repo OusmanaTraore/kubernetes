7b10550cc7370b4de91db7159d46b40c19a85931
OusmanaTraore <60136087+OusmanaTraore@users.noreply.github.com>
2022-03-10 07:12:00 +0100

    Update kubectl-install_3-3.sh

--- kubectl-install_3-3.sh
+++ kubectl-install_3-3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#IP_ETH1="192.168.50.20"
+IP_ETH1="192.168.50.20"
 echo "======      ETAPE 7/9   ========="
 echo " configuration kubernetes"
 sleep 2
@@ -7,23 +7,38 @@ sleep 2
 #sudo echo KUBELET_EXTRA_ARGS="--node-ip=$IP_ETH1" > "/etc/default/kubelet"
 sudo cat "/etc/default/kubelet"
 
-echo "======      ETAPE 8/9   ========="
-echo "Redémarrage des services daemon  et kubelet"
-sleep 2
+if [ $? -ne  KUBELET_EXTRA_ARGS="--node-ip=$IP_ETH1"  ] 
+then
+  echo "Défaut de configuration du fichier /etc/default/kubelet"
+  exit 
+elif
+  echo "======      ETAPE 8/9   ========="
+  echo "Redémarrage des services daemon  et kubelet"
+  sleep 2
 
-echo " 1/2 - Redémarrage daemon"
-systemctl  daemon-reload
-sudo systemctl status daemon-reload
-sleep 2
+  echo " 1/2 - Redémarrage daemon"
+  systemctl  daemon-reload
+  if [ $? -ne 0 ]
+  then
+    echo "ECHEC daemon-reload"
+    exit
+  elif 
+    sudo systemctl status daemon-reload
+    sleep 2
+    echo " 2/2 - Redémarrage kubelet"
+    sudo systemctl restart kubelet
+    sudo systemctl status  kubelet
+    sleep 1
+
+    echo ""
+    echo ""
+    echo "======      ETAPE 9/9   ========="
+    echo " Initialisation de Kubeadm "
+    sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$IP_ETH1  --ignore-preflight-errors=NumCPU
+   fi
+
+fi
 
 
-echo " 2/2 - Redémarrage kubelet"
-sudo systemctl restart kubelet
-sudo systemctl status  kubelet
-sleep 1
 
-echo ""
-echo ""
-echo "======      ETAPE 9/9   ========="
-echo " Initialisation de Kubeadm "
-sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.205.10  --ignore-preflight-errors=NumCPU
+
